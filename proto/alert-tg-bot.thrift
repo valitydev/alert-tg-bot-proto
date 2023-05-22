d98b7f6cbf2087ef519b9fbf570d31c2e8a3f237
Egor Cherniak <cherniak3@yandex.ru>
2023-05-22 10:33:46 +0300

    BUS-23:Impl (#1)
    * BUS-23:Impl
    
    * Fix package name

---
+++ proto/alert-tg-bot.thrift
@@ -0,0 +1,25 @@
+namespace java dev.vality.alerting.tg_bot
+
+/** Идентификатор */
+typedef string ID
+
+struct Notification {
+    /** Уникальный идентификатор уведомления **/
+    1: required ID id
+    /** Идентификатор получателя **/
+    2: required ID receiver_id
+     /** Текст уведомления **/
+    3: required string message
+}
+
+exception ReceiverNotFound {}
+
+/**
+* Интерфейс для отправки уведомлений
+*/
+service NotifierService {
+
+     /** Отправить уведомление **/
+    void Notify (1: Notification notification) throws (1: ReceiverNotFound ex);
+
+}
